f1812f0f37a52c0ef08f9a8b678122e7891ba721
StephanieKathrynGuest <sguest@mail.csuchico.edu>
2016-09-26 11:55:58 -0700

    Added more tests for isSafe and get fuctions

--- FieldTest.cpp
+++ FieldTest.cpp
@@ -22,3 +22,25 @@ TEST(FieldTest, placeMineInBounds)
 	minefield.placeMine(4,5);
 	ASSERT_EQ( MINE_HIDDEN, minefield.get(4,5) );
 }
+TEST(FieldTest, placeMineOutBounds)
+{
+	Field minefield;
+	minefield.placeMine(4,5);
+	ASSERT_EQ( EMPTY_HIDDEN, minefield.get(3,4));
+}
+TEST(FieldTest, isMineSafeYesOnEdge)
+{
+	Field minefield;
+
+	minefield.placeMine(4,5);
+	ASSERT_EQ( false , minefield.isSafe(4,5) );
+}
+
+TEST(FieldTest, isMineSafeNo)
+{
+	Field minefield;
+
+	minefield.placeMine(4,5);
+	ASSERT_EQ( true, minefield.isSafe(5,6) );
+}
+
